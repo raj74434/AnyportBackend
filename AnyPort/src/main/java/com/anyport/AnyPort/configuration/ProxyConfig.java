450595eb22b75ac52145533987f87c149a073878
Raj Gupta <105916680+raj74434@users.noreply.github.com>
2023-07-09 14:21:36 +0530

    Update ProxyConfig.java

--- AnyPort/src/main/java/com/anyport/AnyPort/configuration/ProxyConfig.java
+++ AnyPort/src/main/java/com/anyport/AnyPort/configuration/ProxyConfig.java
@@ -9,7 +9,7 @@ public class ProxyConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("**")
                 .allowedOrigins("https://anyport.netlify.app") // Replace with your Netlify app's URL
                 .allowedMethods("*")
                 .allowedHeaders("*")
